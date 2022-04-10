95bb02426226418c840e23f13dee10ca00cfe574
soulslurpee <charles.d.thompson87@gmail.com>
2022-04-09 21:23:56 -0400

    refactored Player.js

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,3 +1,5 @@
+const { PROPERTY_TYPES } = require('@babel/types');
+const { test, expect } = require('@jest/globals');
 const Player = require('../lib/Player.js');
 const Potion = require('../lib/Potion');
 
@@ -13,3 +15,21 @@ test('creates a player object', () => {
   expect(player.inventory).toEqual(expect.arrayContaining([expect.any(Object)]));
 });
 
+test("gets player's stats as an object", () => {
+  const player = new Player('Dave');
+
+  expect(player.getStats()).toHaveProperty('potions');
+  expect(player.getStats()).toHaveProperty('health');
+  expect(player.getStats()).toHaveProperty('strength');
+  expect(player.getStats()).toHaveProperty('agility');
+});
+
+test('gets inventory from player or returns false', () => {
+  const player = new Player('Dave');
+
+  expect(player.getInventory()).toEqual(expect.any(Array));
+
+  player.inventory = [];
+
+  expect(player.getInventory()).toEqual(false);
+});
