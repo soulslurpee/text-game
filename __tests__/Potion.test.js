7aa2b8dbaf94b2566dfa2694525c603a5c9d3262
soulslurpee <charles.d.thompson87@gmail.com>
2022-04-09 20:54:11 -0400

    added Player constructor and test

--- __tests__/Potion.test.js
+++ __tests__/Potion.test.js
@@ -3,8 +3,8 @@ const Potion = require('../lib/Potion.js');
 test('creates a health potion object', () => {
   const potion = new Potion('health');
 
-  expect (potion.name).toBe('health');
-  expect (potion.value).toEqual(expect.any(Number));
+  expect(potion.name).toBe('health');
+  expect(potion.value).toEqual(expect.any(Number));
 });
 
 test('creates a random potion object', () => {
